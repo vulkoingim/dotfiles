ca01e2e24bdda1c653f941be0abd6d06faf6b2a9
Aleksandar Aleksandrov <vulko@ingim.io>
2021-01-24 20:57:00 +0000

    up

--- install.sh
+++ install.sh
@@ -1,15 +1,23 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
 brew update
 brew upgrade
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+bToInstall=(aom autoconf autojump automake awscli bandwhich bash bat bison cloc cmake coreutils cscope csshx direnv docbook docbook-xsl docker-credential-helper-ecr dog fping freetype fzf gdbm gettext ghostscript git git-crypt git-lfs glib gnu-getopt gnu-sed gradle grep gron helm icdiff icu4c ilmbase imagemagick jmeter jpeg jq kops krb5 kubernetes-cli libde265 libffi libheif libidn2 liblqr libomp libpng libtiff libtool libunistring libxml2 libyaml little-cms2 lua macvim md5sha1sum minikube mysql ncurses node oniguruma openexr openjdk openjpeg openssl@1.1 packer parallel pcre pcre2 pigz pkg-config postgresql protobuf protobuf@3.7 python@2 python@3.8 python@3.9 readline ripgrep ripgrep-all ruby shared-mime-info shellcheck sqlite tcl-tk telnet tfenv tig watch webp wget x265 xmlto xsv xz zlib zsh-syntax-highlighting paperkey qrencode zig)
 
-bToInstall = (aom autoconf autojump automake aws-sam-cli awscli bandwhich bash bat bison cloc cmake coreutils cscope csshx direnv docbook docbook-xsl docker-credential-helper-ecr dog fping freetype fzf gdbm gettext ghostscript git git-crypt git-lfs glib gnu-getopt gnu-sed go-swagger gradle grep gron helm icdiff icu4c ilmbase imagemagick jmeter jpeg jq kops krb5 kubernetes-cli libde265 libffi libheif libidn2 liblqr libomp libpng libtiff libtool libunistring libxml2 libyaml little-cms2 lua macvim md5sha1sum minikube mysql ncurses node oniguruma openexr openjdk openjpeg openssl@1.1 packer parallel pcre pcre2 pigz pkg-config postgresql protobuf protobuf@3.7 python@2 python@3.8 python@3.9 readline ripgrep ripgrep-all ruby shared-mime-info shellcheck sqlite tcl-tk telnet tfenv tig watch webp wget x265 xmlto xsv xz zlib zsh-syntax-highlighting)
+bCaskToInstall=(aws-vault google-cloud-sdk gpg-suite java maccy multipass vagrant virtualbox aws-sam-cli)
 
-bCaskToInstall = (aws-vault google-cloud-sdk gpg-suite java maccy multipass vagrant virtualbox)
+for i in "${bToInstall[@]}";do
+	brew install $i
+done
 
-brew install zsh-syntax-highlighting
+for i in "${bCaskToInstall[@]}";do
+   brew cask install $i
+done
 
 git clone git@github.com:geometry-zsh/geometry.git ~/.oh-my-zsh/themes/
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -18,7 +26,6 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 curl -L https://iterm2.com/shell_integration/zsh -o ~/.iterm2_shell_integration.zsh
 
 # FZF
-brew install fzf
 $(brew --prefix)/opt/fzf/install
 
 # Vim
@@ -28,41 +35,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # install rust
 curl https://sh.rustup.rs -sSf | sh
 
-# direnv per directory environments
-brew install direnv
-# terraform environments
-brew install tfenv
-
-brew cask install multipass
-brew cask install virtualbox
-
-# git tree view
-brew install tig
-
-# bat (CAT), rg, xsv
-brew install bat
-brew install ripgrep
-brew install xsv
-brew install autojump
-
-# crypto
-brew install openssl
-brew cask install gpg-suite
-brew install git-crypt
-
 curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-269.0.0-darwin-x86_64.tar.gz | sudo tar xzf - | ./google-cloud-sdk/install.sh
-
-brew install coreutils
-brew install git-lfs
-brew install imagemagick 
-brew install pigz
-brew install grep
-brew install telnet
-brew isntall python 
-
-brew install kops
-brew install awscli
-brew cask install aws-vault
 
 sudo xcodebuild -license accept
 
@@ -72,6 +45,7 @@ files=(".gitconfig" ".profile" ".bash_profile" ".vimrc" ".zprofile" ".zshenv" ".
 for i in "${files[@]}";do
 	cp $i ~/
 done
+
 # Keka https://www.keka.io/en/
 # BetterTouchTool https://folivora.ai/
 # Docker.dmg
